bdcea3231e215bb2f8b68f09f0715a23f6e0ee85
DoggyDoggyDoggy <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-01-26 02:03:53 +1300

    Custom Layout

---
+++ CustomLayout/app/src/main/java/denys/diomaxius/customlayout/MainActivity.kt
@@ -0,0 +1,75 @@
+package denys.diomaxius.customlayout
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import denys.diomaxius.customlayout.ui.theme.CustomLayoutTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CustomLayoutTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MainScreen(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen(modifier: Modifier = Modifier) {
+    Box(modifier) {
+        CascadeLayout(spacing = 35) {
+            Box(modifier = Modifier.size(60.dp).background(Color.Blue))
+            Box(modifier = Modifier.size(80.dp, 40.dp).background(Color.Red))
+            Box(modifier = Modifier.size(90.dp, 100.dp).background(Color.Cyan))
+            Box(modifier = Modifier.size(50.dp).background(Color.Magenta))
+            Box(modifier = Modifier.size(70.dp).background(Color.Green))
+        }
+    }
+}
+
+@Composable
+fun CascadeLayout(
+    modifier: Modifier = Modifier,
+    spacing: Int = 0,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+        var indent = 0
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            var yCoord = 0
+            val placeables = measurables.map { measurable ->
+                measurable.measure(constraints)
+            }
+
+            placeables.forEach { placeable ->
+                placeable.placeRelative(x = indent, y = yCoord)
+                indent += placeable.width + spacing
+                yCoord += placeable.height + spacing
+            }
+        }
+    }
+}
